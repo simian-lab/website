513165c69e273b388c4c046b199963eccdfc4aa3
Pablo <pablo@puntomob.com>
2014-02-05 17:44:24 -0500

    Multilenguaje con angular-translate. Se pasan las variables por el HTML a la tabla de traducciones al final

--- app.js
+++ app.js
@@ -10,4 +10,33 @@ angular.module('simian', ['ui.router'])
     url: '/',
     templateUrl: '/home/home.html'
   })
-})
+});
+var translationsES = {
+	HEADLINE: 'Esta es a prueba del nuevo Sitio de Simian ',
+	PARAGRAPH: 'Un mono programador esta trabajando en esta cosa',
+	VARIABLE_REPLACEMENT: 'Hola, {{name}}'
+};
+var translationsEN = {
+	HEADLINE: 'This is a trial of the new Simian Site',
+	PARAGRAPH: 'A monkey coder is working on this thing',
+	VARIABLE_REPLACEMENT: 'Hi, {{name}}'
+};
+var app = angular.module('simian', ['pascalprecht.translate']);
+app.config(function ($locationProvider) {
+  $locationProvider.html5Mode(true);
+});
+app.config(['$translateProvider', function ($translateProvider) {
+  // add translation table
+  $translateProvider.translations('en', translationsEN);
+  $translateProvider.translations('es', translationsES);
+  $translateProvider.preferredLanguage('es');
+  $translateProvider.fallbackLanguage('es');;
+}]);
+
+app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
+
+  $scope.changeLanguage = function (langKey) {
+    $translate.uses(langKey);
+    $translate()
+  };
+}]);
