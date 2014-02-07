7807063786312c22d64cb67c386e2f66058a08f2
Pablo <pablo@puntomob.com>
2014-02-07 11:35:36 -0500

    Esqueleto sacado de beta.simian.co (Lenguaje por defecto: Ingles)

--- app/app.js
+++ app/app.js
@@ -12,14 +12,38 @@ angular.module('simian', ['ui.router'])
   })
 });
 var translationsES = {
-	HEADLINE: 'Esta es a prueba del nuevo Sitio de Simian ',
-	PARAGRAPH: 'Un mono programador esta trabajando en esta cosa',
-	VARIABLE_REPLACEMENT: 'Hola, {{name}}'
+	HEADLINE_MAIN: 'Construimos tecnología para seres humanos',
+	PARAGRAPH: 'Normally, both your asses would be dead as fucking fried chicken,'
+  + ' but you happen to pull this shit while Im in a transitional period so I dont'
+  + ' wanna kill you, I wanna help you. But I cant give you this case, it dont'
+  + ' belong to me. Besides, Ive already been through too much shit this morning'
+  + ' over this case to hand it over to your dumb ass.',
+	VARIABLE_REPLACEMENT: 'Hola, {{name}}',
+  HEADLINE_ONE: 'Visión',
+  HEADLINE_TWO: 'Análisis',
+  HEADLINE_THREE: 'Desarrollo',
+  HEADLINE_FOUR: 'Mantenimiento',
+  HEADLINE_FIVE: 'Trabajo Reciente',
+  HEADLINE_SIX: 'Nuestros clientes dicen',
+  HEADLINE_SEVEN: 'Nuestro proceso',
+  HEADLINE_EIGHT: 'El equipo'
 };
 var translationsEN = {
-	HEADLINE: 'This is a trial of the new Simian Site',
-	PARAGRAPH: 'A monkey coder is working on this thing',
-	VARIABLE_REPLACEMENT: 'Hi, {{name}}'
+	HEADLINE_MAIN: 'We build tecnology for human beings',
+	PARAGRAPH: 'Normally, both your asses would be dead as fucking fried chicken,'
+  + ' but you happen to pull this shit while Im in a transitional period so I dont'
+  + ' wanna kill you, I wanna help you. But I cant give you this case, it dont'
+  + ' belong to me. Besides, Ive already been through too much shit this morning'
+  + ' over this case to hand it over to your dumb ass.',
+	VARIABLE_REPLACEMENT: 'Hi, {{name}}',
+  HEADLINE_ONE: 'Vision',
+  HEADLINE_TWO: 'Analysis',
+  HEADLINE_THREE: 'Development',
+  HEADLINE_FOUR: 'Maintenance',
+  HEADLINE_FIVE: 'Recent Work',
+  HEADLINE_SIX: 'Our Clients Say',
+  HEADLINE_SEVEN: 'Our Process',
+  HEADLINE_EIGHT: 'The Team'
 };
 var app = angular.module('simian', ['pascalprecht.translate']);
 app.config(function ($locationProvider) {
@@ -29,8 +53,8 @@ app.config(['$translateProvider', function ($translateProvider) {
   // add translation table
   $translateProvider.translations('en', translationsEN);
   $translateProvider.translations('es', translationsES);
-  $translateProvider.preferredLanguage('es');
-  $translateProvider.fallbackLanguage('es');;
+  $translateProvider.preferredLanguage('en');
+  $translateProvider.fallbackLanguage('en');;
 }]);
 
 app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
