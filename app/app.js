4f5f9a6b43d1144e2a05f99d7bec6c91c8881675
Pablo <pablo@puntomob.com>
2014-02-07 17:12:06 -0500

    Meta tags on index.html

--- app/app.js
+++ app/app.js
@@ -1,11 +1,8 @@
 'use strict';
-
-angular.module('simian', ['ui.router'])
-
-.config(function($stateProvider, $locationProvider) {
+var routerUI = angular.module('simian', ['ui.router']);
+routerUI.config(function($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $locationProvider.hashPrefix('!');
-
   $stateProvider.state('home', {
     url: '/',
     templateUrl: '/home/home.html'
