93ffed002db47ea915e81bed3bd1324586c70c3a
Sergio Acosta <acosta.checho@gmail.com>
2014-02-09 22:11:22 -0500

    Organized the Pablo's code to get the first version of the site working with UI-Router and Angular-Translate

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
 'use strict';
 
-angular.module('simian', ['ui.router'])
+angular.module('simian', ['ui.router', 'pascalprecht.translate'])
 
-.config(function($stateProvider, $locationProvider) {
+.config(function($stateProvider, $locationProvider, $translateProvider) {
   $locationProvider.html5Mode(true);
   $locationProvider.hashPrefix('!');
 
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl: '/home/home.html'
-  })
-});
-var translationsES = {
-	HEADLINE_MAIN: 'Construimos tecnología para seres humanos',
-	PARAGRAPH: 'Normally, both your asses would be dead as fucking fried chicken,'
-  + ' but you happen to pull this shit while Im in a transitional period so I dont'
-  + ' wanna kill you, I wanna help you. But I cant give you this case, it dont'
-  + ' belong to me. Besides, Ive already been through too much shit this morning'
-  + ' over this case to hand it over to your dumb ass.',
-	VARIABLE_REPLACEMENT: 'Hola, {{name}}',
-  HEADLINE_ONE: 'Visión',
-  HEADLINE_TWO: 'Análisis',
-  HEADLINE_THREE: 'Desarrollo',
-  HEADLINE_FOUR: 'Mantenimiento',
-  HEADLINE_FIVE: 'Trabajo Reciente',
-  HEADLINE_SIX: 'Nuestros clientes dicen',
-  HEADLINE_SEVEN: 'Nuestro proceso',
-  HEADLINE_EIGHT: 'El equipo'
-};
-var translationsEN = {
-	HEADLINE_MAIN: 'We build tecnology for human beings',
-	PARAGRAPH: 'Normally, both your asses would be dead as fucking fried chicken,'
-  + ' but you happen to pull this shit while Im in a transitional period so I dont'
-  + ' wanna kill you, I wanna help you. But I cant give you this case, it dont'
-  + ' belong to me. Besides, Ive already been through too much shit this morning'
-  + ' over this case to hand it over to your dumb ass.',
-	VARIABLE_REPLACEMENT: 'Hi, {{name}}',
-  HEADLINE_ONE: 'Vision',
-  HEADLINE_TWO: 'Analysis',
-  HEADLINE_THREE: 'Development',
-  HEADLINE_FOUR: 'Maintenance',
-  HEADLINE_FIVE: 'Recent Work',
-  HEADLINE_SIX: 'Our Clients Say',
-  HEADLINE_SEVEN: 'Our Process',
-  HEADLINE_EIGHT: 'The Team'
-};
-var app = angular.module('simian', ['pascalprecht.translate']);
-app.config(function ($locationProvider) {
-  $locationProvider.html5Mode(true);
-});
-app.config(['$translateProvider', function ($translateProvider) {
-  // add translation table
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/home/home.html'
+    })
+    .state('enter', {
+      url: '/enter',
+      templateUrl: '/enter/enter.html'
+    })
+    .state('kienyke', {
+      url: '/kienyke',
+      templateUrl: '/kienyke/kienyke.html'
+    });
+
+
+  var translationsES = {
+    HEADLINE_MAIN: 'Construimos tecnología para seres humanos',
+    PARAGRAPH: 'Normally, both your asses would be dead as fucking fried chicken,'
+    + ' but you happen to pull this shit while Im in a transitional period so I dont'
+    + ' wanna kill you, I wanna help you. But I cant give you this case, it dont'
+    + ' belong to me. Besides, Ive already been through too much shit this morning'
+    + ' over this case to hand it over to your dumb ass.',
+    VARIABLE_REPLACEMENT: 'Hola, {{name}}',
+    HEADLINE_ONE: 'Visión',
+    HEADLINE_TWO: 'Análisis',
+    HEADLINE_THREE: 'Desarrollo',
+    HEADLINE_FOUR: 'Mantenimiento',
+    HEADLINE_FIVE: 'Trabajo Reciente',
+    HEADLINE_SIX: 'Nuestros clientes dicen',
+    HEADLINE_SEVEN: 'Nuestro proceso',
+    HEADLINE_EIGHT: 'El equipo'
+  };
+  var translationsEN = {
+    HEADLINE_MAIN: 'We build tecnology for human beings',
+    PARAGRAPH: 'Normally, both your asses would be dead as fucking fried chicken,'
+    + ' but you happen to pull this shit while Im in a transitional period so I dont'
+    + ' wanna kill you, I wanna help you. But I cant give you this case, it dont'
+    + ' belong to me. Besides, Ive already been through too much shit this morning'
+    + ' over this case to hand it over to your dumb ass.',
+    VARIABLE_REPLACEMENT: 'Hi, {{name}}',
+    HEADLINE_ONE: 'Vision',
+    HEADLINE_TWO: 'Analysis',
+    HEADLINE_THREE: 'Development',
+    HEADLINE_FOUR: 'Maintenance',
+    HEADLINE_FIVE: 'Recent Work',
+    HEADLINE_SIX: 'Our Clients Say',
+    HEADLINE_SEVEN: 'Our Process',
+    HEADLINE_EIGHT: 'The Team'
+  };
+
   $translateProvider.translations('en', translationsEN);
   $translateProvider.translations('es', translationsES);
   $translateProvider.preferredLanguage('en');
   $translateProvider.fallbackLanguage('en');;
-}]);
+})
 
-app.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
+.controller('Ctrl', ['$translate', '$scope', function ($translate, $scope) {
 
   $scope.changeLanguage = function (langKey) {
     $translate.uses(langKey);
