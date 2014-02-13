fe4b118def26397e8bd182c4c541fc8298b81ebc
Ivan Vasquez <ivancamilov@gmail.com>
2014-02-13 12:28:52 -0500

    - Fixed grunt-plato missing reference - Added jshint to the build process - Improved general grunt config so only required files are processed.

--- topbar/topbar.js
+++ topbar/topbar.js
@@ -2,10 +2,13 @@
 
 angular.module('simian.topbar', [])
 
-.controller('topbarController', function($stateProvider) {})
+.controller('topbarController', function() {
+  // TODO: something
+})
 
 .directive('topbar', function() {
   return {
     templateUrl: '/topbar/topbar.html'
-  }
+  };
 })
+;
