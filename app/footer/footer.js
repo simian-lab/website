5409ba74b18a742792038cc7841b3bf479c86a07
nicolasaiz173 <nicolasaiz173@gmail.com>
2014-02-13 17:07:53 -0500

    Improved the responsive-design behavior.

---
+++ app/footer/footer.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('simian.footer', [])
+
+.controller('footerController', function() {
+  // TODO: something
+})
+
+.directive('footer', function() {
+  return {
+    templateUrl: '/footer/footer.html'
+  };
+})
+;
