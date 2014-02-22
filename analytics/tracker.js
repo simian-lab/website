c7b921ffc2e770733924847b3a0a75fdafba6ba3
nicolasaiz173 <nicolasaiz173@gmail.com>
2014-02-21 20:15:19 -0500

    Yuidoc temporarily disabled.

--- analytics/tracker.js
+++ analytics/tracker.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('simian.tracker', ['simian.configuration'])
-  .factory('AnalyticsTracker',function($rootScope,$window) {
-    return {
-      initGATracker: function(){
-        var domainId;
-        switch($rootScope.ENVIRONMENT){
+.factory('AnalyticsTracker',function($rootScope,$window) {
+  return {
+    initGATracker: function(){
+      var domainId;
+      switch($rootScope.ENVIRONMENT){
         case 'simian':
           domainId ='UA-48202840-1';
           break;
@@ -19,25 +19,26 @@ angular.module('simian.tracker', ['simian.configuration'])
           domainId ='UA-48202840-4';
           break;
       }
-        $window.ga('create', domainId, 'simian.co');
-      },
-      trackGAPageview: function(path){
-        $window.ga('send', 'pageview', path);
-      },
-      trackGAEvent: function(category, action, label){
-        $window.ga('send', 'event', category, action, label);
-      },
-      pageTrack: function(path) {
-        // We set this in a timeout so we know for sure the DOM
-        // has been updated with the correct meta-shtick
-        var self = this;
+      $window.ga('create', domainId, 'simian.co');
+    },
+    trackGAPageview: function(path){
+      $window.ga('send', 'pageview', path);
+    },
+    trackGAEvent: function(category, action, label){
+      $window.ga('send', 'event', category, action, label);
+    },
+    pageTrack: function(path) {
+      // We set this in a timeout so we know for sure the DOM
+      // has been updated with the correct meta-shtick
+      var self = this;
 
-        setTimeout(function() {
-          self.trackGAPageview(path);
-        }, 10);
-      },
-      eventTrack: function(category, action, label) {
-        this.trackGAEvent(category, action, label);
-      }
-    };
-  });
+      setTimeout(function() {
+        self.trackGAPageview(path);
+      }, 10);
+    },
+
+    eventTrack: function(category, action, label) {
+      this.trackGAEvent(category, action, label);
+    }
+  };
+});
