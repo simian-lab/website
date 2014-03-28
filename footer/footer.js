0a4eaba21c6f50ddcc9c5af94fea370e27036e2d
Pablo <pablo@puntomob.com>
2014-03-28 14:08:45 -0500

    Fixed a translate problem on footer

--- footer/footer.js
+++ footer/footer.js
@@ -43,7 +43,7 @@ angular.module('simian.footer', ['simian.configuration'])
   $translateProvider.fallbackLanguage('en');
 })
 
-.controller('footerController', function($scope, AnalyticsTracker) {
+.controller('footerController', function($rootScope, $scope, AnalyticsTracker) {
   // TODO: something
   var CONTACT_ROUTE = $rootScope.CONTACT_ROUTE;
   $scope.sendForm = function(){
