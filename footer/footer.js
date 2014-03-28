280e3c1d13697ed679c8dd20292fe555753ebe16
Ivan Vasquez <ivancamilov@gmail.com>
2014-03-28 16:14:38 -0500

    - Env and config settings changed (sorry if it messes up something!) - Fixed contact form naming issues - Added better status codes for the mailer

--- footer/footer.js
+++ footer/footer.js
@@ -43,20 +43,20 @@ angular.module('simian.footer', ['simian.configuration'])
   $translateProvider.fallbackLanguage('en');
 })
 
-.controller('footerController', function($scope, AnalyticsTracker) {
+.controller('footerController', function($scope, $rootScope, $http, AnalyticsTracker) {
   // TODO: something
   var CONTACT_ROUTE = $rootScope.CONTACT_ROUTE;
   $scope.sendForm = function(){
     AnalyticsTracker.eventTrack('button', 'click', 'contact button');
     $http.post(CONTACT_ROUTE,{
-      Name: contact.name,
-      Email: contact.email,
-      Message: contact.message,
-      captcha: contact.hidden
+      Name: $scope.name,
+      Email: $scope.email,
+      Message: $scope.message,
+      captcha: $scope.hidden
     }).success(function (data, status, headers, config) {
-      // TODO
+      alert('success!');
     }).error(function (data, status, headers, config) {
-      // TODO
+      alert('failure :(');
     });
   };
 })
