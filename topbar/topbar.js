2cb06a6e98be624e21cdab55f641308869443a28
Ivan Vasquez <ivancamilov@gmail.com>
2014-04-02 15:09:55 -0500

    Worked on SMN-28.
    Topbar.js
    - Relocated menu functions to the linking function instead of the controllers.
    - Removed mobilecheck function; it's useless and counterproductive.
    - Added whitespace for readability
    - Added documentation
    - Added analytics tracking
      - Added event for menu opening
      - Added event for menu closing
    - Changed the location of the <nav>. It's now inside the topbar.
    - Removed unnecessary HTML elements (container and wrapper) in the index.html.

--- topbar/topbar.js
+++ topbar/topbar.js
@@ -11,180 +11,40 @@
  */
 angular.module('simian.topbar', [])
 
-.controller('topbarController', function() {
-
-  var hasClass, addClass, removeClass;
+.controller('topbarController', function($scope, AnalyticsTracker) {
 
   /**
   * @doc function
-  * @name topbar.function:classReg
-  * @param  {string} string_in any ol' string
-  * @return {string} adds on the 'izzle'
+  * @name topbar.controller:trackOpenMenu
   * @description
-  * TODO: Complete this.
+  * Tracks an event in analytics when the menu is opened.
   */
-  function classReg( className ) {
-    return new RegExp('(^|\\s+)' + className + '(\\s+|$)');
-  }
-
-  if ( 'classList' in document.documentElement ) {
-    hasClass = function( elem, c ) {
-      return elem.classList.contains( c );
-    };
-    addClass = function( elem, c ) {
-      elem.classList.add( c );
-    };
-    removeClass = function( elem, c ) {
-      elem.classList.remove( c );
-    };
-  }
-  else {
-    hasClass = function( elem, c ) {
-      return classReg( c ).test( elem.className );
-    };
-    addClass = function( elem, c ) {
-      if ( !hasClass( elem, c ) ) {
-        elem.className = elem.className + ' ' + c;
-      }
-    };
-    removeClass = function( elem, c ) {
-      elem.className = elem.className.replace( classReg( c ), ' ' );
-    };
-  }
-
-  /**
-  * @doc function
-  * @name topbar.function:toggleClass
-  * @param  {string} string_in any ol' string
-  * @return {string} adds on the 'izzle'
-  * @description
-  * TODO: Complete this.
-  */
-  function toggleClass( elem, c ) {
-    var fn = hasClass( elem, c ) ? removeClass : addClass;
-    fn( elem, c );
-  }
-
-  var classie = {
-    // full names
-    hasClass: hasClass,
-    addClass: addClass,
-    removeClass: removeClass,
-    toggleClass: toggleClass,
-    // short names
-    has: hasClass,
-    add: addClass,
-    remove: removeClass,
-    toggle: toggleClass
+  $scope.trackOpenMenu = function() {
+    AnalyticsTracker.eventTrack('Navigation', 'Menu opened');
   };
 
-  // transport
-  if ( typeof define === 'function' && define.amd ) {
-    // AMD
-    define( classie );
-  } else {
-    // browser global
-    window.classie = classie;
-  }
+  /**
+  * @doc function
+  * @name topbar.controller:trackCloseMenu
+  * @description
+  * Tracks an event in analytics when the menu is closed.
+  */
+  $scope.trackCloseMenu = function() {
+    AnalyticsTracker.eventTrack('Navigation', 'Menu closed');
+  };
 
   /**
   * @doc function
-  * @name topbar.function:scrollY
-  * @param  {string} string_in any ol' string
-  * @return {string} adds on the 'izzle'
+  * @name topbar.controller:trackNavigation
+  @ param {string} label for the link the user clicked on
   * @description
-  * TODO: Complete this.
+  * Tracks an event in analytics when the user clicks on the navigation
+  * links in the menu.
   */
-  function scrollY() {
-    return window.pageYOffset || docElem.scrollTop;
-  }
+  $scope.trackNavigation = function(link) {
+    AnalyticsTracker.eventTrack('Navigation', 'Menu link clicked', link);
+  };
 
-  /**
-  * @doc function
-  * @name topbar.function:mobilecheck
-  * @param  {string} string_in any ol' string
-  * @return {string} adds on the 'izzle'
-  * @description
-  * TODO: Complete this.
-  */
-  function mobilecheck() {
-    var check = false;
-    (function(a){if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-  }
-
-  var docElem = window.document.documentElement,
-  // support transitions
-  support = Modernizr.csstransitions,
-  // transition end event name
-  transEndEventNames = {
-    'WebkitTransition': 'webkitTransitionEnd',
-    'MozTransition': 'transitionend',
-    'OTransition': 'oTransitionEnd',
-    'msTransition': 'MSTransitionEnd',
-    'transition': 'transitionend'
-  },
-  transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-  docscroll = 0,
-  // click event (if mobile use touchstart)
-  clickevent = mobilecheck() ? 'touchstart' : 'click';
-
-  /**
-  * @doc function
-  * @name topbar.function:init
-  * @param  {string} string_in any ol' string
-  * @return {string} adds on the 'izzle'
-  * @description
-  * TODO: Complete this.
-  */
-  function init() {
-    var showMenu = document.getElementById( 'showMenu' ),
-    perspectiveWrapper = document.getElementById( 'perspective' ),
-    container = perspectiveWrapper.querySelector( '.container' ),
-    contentWrapper = container.querySelector( '.wrapper' );
-
-    showMenu.addEventListener( clickevent, function( ev ) {
-      ev.stopPropagation();
-      ev.preventDefault();
-      docscroll = scrollY();
-      // change top of contentWrapper
-      contentWrapper.style.top = docscroll * -1 + 'px';
-      // mac chrome issue:
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
-      // add modalview class
-      classie.add( perspectiveWrapper, 'modalview' );
-      // animate..
-      setTimeout( function() { classie.add( perspectiveWrapper, 'animate' ); }, 25 );
-    });
-
-    container.addEventListener( clickevent, function( ev ) {
-      if( classie.has( perspectiveWrapper, 'animate') ) {
-        var onEndTransFn = function( ev ) {
-          if( support && ( ev.target.className !== 'container' || ev.propertyName.indexOf( 'transform' ) === -1 ) ) {
-            return;
-          }
-          this.removeEventListener( transEndEventName, onEndTransFn );
-          classie.remove( perspectiveWrapper, 'modalview' );
-          // mac chrome issue:
-          document.body.scrollTop = document.documentElement.scrollTop = docscroll;
-          // change top of contentWrapper
-          contentWrapper.style.top = '0px';
-        };
-        if( support ) {
-          perspectiveWrapper.addEventListener( transEndEventName, onEndTransFn );
-        }
-        else {
-          onEndTransFn.call();
-        }
-        classie.remove( perspectiveWrapper, 'animate' );
-      }
-    });
-
-    perspectiveWrapper.addEventListener( clickevent, function( ev ) {
-      return false; } );
-  }
-
-  init();
 })
 
 /**
@@ -199,10 +59,111 @@ angular.module('simian.topbar', [])
     controller: 'topbarController',
     replace: true,
     link: function($scope, $element, $attrs) {
-      angular.element($window).bind('scroll', function() {
-        $scope.scrolled = true;
-      });
+      var showMenu = angular.element(document.getElementById( 'showMenu' )),
+      perspectiveWrapper = angular.element(document.getElementById( 'perspective' )),
+      navigation = angular.element(document.getElementById( 'navigation' )),
+      container = angular.element(document.getElementById( 'perspective-container' ));
 
+      function renestNavigationElement() {
+        perspectiveWrapper.append(navigation.remove());
+      };
+
+      /**
+      * @doc function
+      * @name topbar.function:scrollY
+      * @description
+      * TODO: Determines how much the page has scrolled?.
+      */
+      function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+      }
+
+      function openMenu(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        docscroll = scrollY();
+
+        // mac chrome issue:
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+        // add modalview class
+        perspectiveWrapper.addClass('modalview');
+
+        // animate...
+        setTimeout(function() {
+          perspectiveWrapper.addClass('animate');
+        }, 25 );
+
+        $scope.trackOpenMenu();
+      };
+
+      function closeMenu(ev) {
+        if(perspectiveWrapper.hasClass('animate')) {
+
+          var onEndTransFn = function( ev ) {
+            if( support && ( ev.target.className !== 'container' || ev.propertyName.indexOf( 'transform' ) === -1 ) ) {
+              return;
+            }
+
+            perspectiveWrapper.unbind( transEndEventName, onEndTransFn );
+            perspectiveWrapper.removeClass('modalview');
+
+            // mac chrome issue:
+            document.body.scrollTop = document.documentElement.scrollTop = docscroll;
+          };
+
+          if( support ) {
+            perspectiveWrapper.bind( transEndEventName, onEndTransFn );
+          }
+          else {
+            onEndTransFn.call();
+          }
+
+          perspectiveWrapper.removeClass('animate');
+          $scope.trackCloseMenu();
+        }
+      };
+
+      /**
+      * @doc function
+      * @name topbar.function:init
+      * @description
+      * TODO: Initialize the menu behavior.
+      */
+      function init() {
+        // Change the location of the menu
+        renestNavigationElement();
+
+        // Bind opening event to the menu button
+        showMenu.bind('click', openMenu);
+
+        // Bind closing event to the main element
+        container.bind('click', closeMenu);
+
+        // Make sure the parent element doesn't do anything when it's clicked.
+        perspectiveWrapper.bind('click', function( ev ) {
+          return false; } );
+
+        // Make sure we identify when the page is scrolled
+        angular.element($window).bind('scroll', function() {
+          $scope.scrolled = true;
+        });
+
+        // If the page has scrolled, then change the topbar style
+        setInterval(function() {
+          if($scope.scrolled) {
+            $scope.scrolled = false;
+            changeTopbarStyle();
+          }
+        }, 100); // If performance becomes an issue, increase this number
+      }
+
+      /**
+       * @doc function
+       * @name topbar.directive:isElementInViewport
+       * @description
+       * Determines whether an element is visible in the viewport or not.
+       */
       var isElementInViewport = function(el) {
         var rect = el.getBoundingClientRect();
 
@@ -214,6 +175,12 @@ angular.module('simian.topbar', [])
         );
       };
 
+      /**
+       * @doc function
+       * @name topbar.directive:changeTopbarStyle
+       * @description
+       * Changes the topbar style when it scrolls.
+       */
       var changeTopbarStyle = function() {
         var logo = document.getElementById('logo');
 
@@ -225,12 +192,21 @@ angular.module('simian.topbar', [])
         $scope.$apply();
       };
 
-      setInterval(function() {
-        if($scope.scrolled) {
-          $scope.scrolled = false;
-          changeTopbarStyle();
-        }
-      }, 100);
+      var docElem = window.document.documentElement,
+      // support transitions
+      support = Modernizr.csstransitions,
+      // transition end event name
+      transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+      },
+      transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+      docscroll = 0;
+
+      init();
     }
   };
  })
