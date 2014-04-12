6acd4ec5cde865eaf23a2c225c68e9ac7e6842a9
Ivan Vasquez <ivancamilov@gmail.com>
2014-04-12 17:28:19 -0500

    - Fixed page routing with trailing slashes - Changed publisher and author info for Google+ - Added a <base href> for html5mode routing - Changed 'what we do' images z-index to avoid issues with text selection and posible links.

--- app/app.js
+++ app/app.js
@@ -22,9 +22,35 @@ angular.module('simian', [
   'simian.tracker'
 ])
 
-.config(function($stateProvider, $locationProvider) {
+.config(function($stateProvider, $locationProvider, $urlRouterProvider) {
   $locationProvider.html5Mode(true);
   $locationProvider.hashPrefix('!');
+
+  // Make sure all routes work with and without trailing slash
+  $urlRouterProvider.rule(function($injector, $location) {
+    var path = $location.path()
+      // Note: misnomer. This returns a query object, not a search string
+      , search = $location.search()
+      , params
+      ;
+
+    // check to see if the path already ends in '/'
+    if (path[path.length - 1] === '/') {
+      return;
+    }
+
+    // If there was no search string / query params, return with a `/`
+    if (Object.keys(search).length === 0) {
+      return path + '/';
+    }
+
+    // Otherwise build the search string and return a `/?` prefix
+    params = [];
+    angular.forEach(search, function(v, k){
+      params.push(k + '=' + v);
+    });
+    return path + '/?' + params.join('&');
+  });
 
   $stateProvider
   .state('home', {
