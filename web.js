4bb8a600e8ef41629dad6836828333eb53deb4bf
Pablo <pablo@puntomob.com>
2014-10-17 11:38:12 -0500

    Web js fixed

--- web.js
+++ web.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
-    next();
-});
-
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/app'));
 
 app.listen(process.env.PORT || 5000);
