65c6f00ceafada5d150ad1f4d2d48a66b8d862de
Pablo <pablo@puntomob.com>
2014-10-21 13:35:55 -0500

    Trying to set encode headers when requesting the .sgvz image

--- web.js
+++ web.js
@@ -3,4 +3,10 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/app'));
 
+// Send encoding header
+app.use('/images/mainPicture.svgz', function (req, res, next) {
+  res.set( 'Content-Type', 'image/svg+xml')
+  res.set( 'Content-Encoding', 'gzip');
+  next();
+})
 app.listen(process.env.PORT || 5000);
