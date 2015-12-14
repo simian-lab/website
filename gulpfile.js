6c43cd02436cc5504d429686b83a5d4c04e74536
PabloAlejandroGarcia <pablo@simian.co>
2015-12-14 17:58:03 -0500

    SMN-122 Works compiling styles

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	bourbon = require('node-bourbon');
+
+gulp.task('sass', function () {
+	gulp.src('app/styles/**.scss')
+	.pipe(sass({
+    includePaths: bourbon.includePaths,
+    outputStyle: 'compressed'
+  }).on('error', sass.logError))
+	.pipe(gulp.dest('app/styles/'))
+});
