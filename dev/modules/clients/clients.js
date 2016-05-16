7eeebfda8da2e04c04822025420c67ad5bcd7c5d
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2016-05-16 15:22:10 -0500

    Directiva para mostrar la grilla de clientes.

--- dev/modules/clients/clients.js
+++ dev/modules/clients/clients.js
@@ -4,8 +4,44 @@ angular.module('simian.clients', [])
 
 .controller('ClientsController', [
   '$scope',
-  function($scope) {
-    console.log('ClientsController');
+  function($scope) {}
+])
+
+.factory('ClientsService', [
+  '$http', '$q',
+  function($http, $q) {
+    return {
+      getClients: function() {
+        var deferred;
+
+        deferred = $q.defer();
+
+        $http.get('json/clients.json').success(function(response) {
+          deferred.resolve(response);
+        });
+
+        return deferred.promise;
+      }
+    }
+  }
+])
+
+.directive('simianClients', [
+  'ClientsService',
+  function(ClientsService) {
+    return {
+      replace: true,
+      restrict: 'E',
+      scope: {
+        limit: '='
+      },
+      templateUrl: '/modules/clients/clients-grid.html',
+      link: function($scope) {
+        ClientsService.getClients().then(function(response) {
+          $scope.clients = response.clients;
+        });
+      }
+    }
   }
 ])
 ;
