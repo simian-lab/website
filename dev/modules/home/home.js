1bbf626f256c6ff8e0833d1f5c916a52d8b7b8b2
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2016-06-23 08:00:49 -0500

    Varios ajustes del home.

--- dev/modules/home/home.js
+++ dev/modules/home/home.js
@@ -3,8 +3,8 @@
 angular.module('simian.home', [])
 
 .controller('HomeController', [
-  '$scope', 'MonkeysService',
-  function($scope, MonkeysService) {
+  '$scope', '$timeout', 'MonkeysService', 'ProjectsService',
+  function($scope, $timeout, MonkeysService, ProjectsService) {
 
     MonkeysService.getMonkeys().then(function(response) {
       $scope.bosses = [ response.monkeys[0], response.monkeys[1] ];
@@ -16,6 +16,20 @@ angular.module('simian.home', [])
         }
       });
     });
+
+    ProjectsService.getProjects().then(function(response) {
+      $scope.projects = response.projects;
+
+      $timeout(function() {
+        var projectsSlider = new Swiper('#projects-slider', {
+          loop: true,
+          nextButton: '.swiper-button-next',
+          prevButton: '.swiper-button-prev',
+          slidesPerView: 3,
+          spaceBetween: 10
+        });
+      })
+    })
   }
 ])
 ;
