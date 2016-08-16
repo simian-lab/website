bdce761690c97e49cabe2aa988c43634c929d283
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2016-08-16 18:05:32 -0500

    Header translation

--- dev/modules/header/header.js
+++ dev/modules/header/header.js
@@ -2,6 +2,26 @@
 
 angular.module('simian.header', [])
 
+.config(['$translateProvider', function($translateProvider) {
+  $translateProvider.translations('en', {
+    'WE-DO': 'We do',
+    'PROJECTS': 'Our projects',
+    'CLIENTS': 'Who has been with us',
+    'WE-ARE': 'Who are we',
+    'LOCATION': 'Find us',
+    'WE-SHARE': 'We share our knowledge'
+  });
+
+  $translateProvider.translations('es', {
+    'WE-DO': 'Qué hacemos',
+    'PROJECTS': 'Nuestros proyectos',
+    'CLIENTS': 'Quiénes han estado con nosotros',
+    'WE-ARE': 'Quiénes somos',
+    'LOCATION': 'Dónde encontrarnos',
+    'WE-SHARE': 'Compartimos lo que sabemos'
+  });
+}])
+
 .controller('HeaderController', [
   '$scope', '$state', '$timeout', 'TranslateService',
   function($scope, $state, $timeout, TranslateService) {
