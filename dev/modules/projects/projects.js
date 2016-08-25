e9be81a28dc6d1ae73dcef5f3d2eafa513940daf
jact1001 <jact1001@gmail.com>
2016-08-24 23:08:00 -0500

    directivas para proyectos mobile, desktop y ipad

--- dev/modules/projects/projects.js
+++ dev/modules/projects/projects.js
@@ -8,8 +8,7 @@ angular.module('simian.projects', [])
   function($scope, ProjectsService) {
     console.log('ProjectsController');
     ProjectsService.getProjects().then(function(response){
-      $scope.projects = response.projects;
-      $scope.project = response.projects[0];
+      $scope.data = response.projects;
     });
 
   }
@@ -33,4 +32,114 @@ angular.module('simian.projects', [])
     }
   }
 ])
+
+.factory('assembler', [ function () {
+  var Assembler = function () {
+    this.getElements = function (data, mode) {
+      var content = '';
+      var direc = '';
+
+      console.log(data[0].type);
+
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].type && (data[i].type == "desktop")) {
+          direc = '<projects-desktop></projects-desktop>';
+        }
+        if (data[i].type && (data[i].type == "mobile")) {
+          direc = '<projects-mobile></projects-mobile>';
+        }
+        if (data[i].type && (data[i].type == "ipad")) {
+          direc = '<projects-ipad></projects-ipad>';
+        }
+        content = content + direc;
+      }
+      return content;
+    };
+  };
+  return new Assembler();
+}
+])
+
+.directive('rAssembler', [ 'assembler', '$compile',
+  function (assembler,$compile) {
+  return {
+    restrict: 'E',
+    scope: {
+      data: '=',
+      mode: "@"
+    },
+    controller: 'ProjectsController',
+    controllerAs: 'ctrl',
+    template: '<span>{{data}}</span>',
+
+
+    link: function (scope, element, attrs) {
+      scope.$watch('data', function() {
+
+        element.html(assembler.getElements(scope.data, scope.mode));
+        $compile(element.contents())(scope);
+      },true);
+    }
+  };
+}
+])
+
+.directive('projectsMobile', [
+  'ProjectsService',
+  function(ProjectsService) {
+    return {
+      replace: true,
+      restrict: 'E',
+      scope: {
+        limit: '='
+      },
+      templateUrl: '/modules/projects/projects-mobile.html',
+      link: function($scope) {
+        ProjectsService.getProjects().then(function(response) {
+          $scope.projects = response.projects;
+        });
+      }
+    }
+  }
+])
+
+.directive('projectsIpad', [
+  'ProjectsService',
+  function(ProjectsService) {
+    return {
+      replace: true,
+      restrict: 'E',
+      scope: {
+        limit: '='
+      },
+      templateUrl: '/modules/projects/projects-ipad.html',
+      link: function($scope) {
+        ProjectsService.getProjects().then(function(response) {
+          $scope.projects = response.projects;
+        });
+      }
+    }
+  }
+])
+
+.directive('projectsDesktop', [
+  'ProjectsService',
+  function(ProjectsService) {
+    return {
+      replace: true,
+      restrict: 'E',
+      scope: {
+        limit: '='
+      },
+      templateUrl: '/modules/projects/projects-desktop.html',
+      link: function($scope) {
+        ProjectsService.getProjects().then(function(response) {
+          $scope.projects = response.projects;
+        });
+      }
+    }
+  }
+])
 ;
+
+
