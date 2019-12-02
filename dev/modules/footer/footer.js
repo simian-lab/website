bfe1c66cf6458bc711c28247ba730acb7c89d134
Alejo Next <alejonext@gmail.com>
2019-12-02 16:27:24 -0500

    Interactive elements

--- dev/modules/footer/footer.js
+++ dev/modules/footer/footer.js
@@ -3,21 +3,33 @@
 angular.module('simian.footer', [])
 
 .controller('FooterController', [
-  '$scope', 'MonkeysService',
-  function($scope, monkeys) {
+  '$scope', 'MonkeysService', '$timeout',
+  function($scope, monkeys, $timeout) {
+    var TIME = 2 * 1000;
     $scope.year = (new Date()).getFullYear();
+
     $scope.submit = function () {
       if($scope.contact.$valid){
+        $scope.clear();
         $scope.contact.$send = true;
+
         monkeys.contactMonkeys($scope.contact.value)
           .then(function (data) {
             $scope.contact.value = {};
-            $scope.contact.$send = false;
+            $scope.setClear = $timeout($scope.clear, TIME);
           })
           .catch(function (err) {
-            $scope.contact.$send = false;
+            $scope.setClear =  $timeout($scope.clear, TIME);
           });
       }
+    };
+
+    $scope.clear = function () {
+      if($scope.setClear && $scope.setClear.cancel){
+        $scope.setClear.cancel();
+      }
+
+      $scope.contact.$send = false;
     };
   }
 ])
